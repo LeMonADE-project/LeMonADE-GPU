29586c10cdf0148f54b56bc5444404c8a93b5114
Toni Mueller <mueller-toni@ipfdd.de>
2019-05-29 10:53:42 +0200

    remove unneeded file

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash 
-rm -r build/
-./configure -DINSTALLDIR_LEMONADEGPU=/home/Toe-Knee/software/lemonadeGPU_install -DLEMONADE_DIR=/home/Toe-Knee/software/lemonade_install  -DCUDA_ARCH=60
-make -j 2
