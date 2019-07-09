ff2f1f3f405d1452be8e55efad9852ddb4d301fa
Toni Mueller <mueller-toni@ipfdd.de>
2019-07-09 11:26:58 +0200

    make it more nice...

--- include/LeMonADEGPU/core/Method.h
+++ include/LeMonADEGPU/core/Method.h
@@ -12,20 +12,26 @@ class Method {
      SpaceFillingCurve curve; 
   
  public:
-    __device__ __host__ const SpaceFillingCurve&   getCurve() const {return curve; }
+    __device__ __host__ const SpaceFillingCurve&   getCurve() const 
+    {return curve; }
 
-   __device__ __host__ SpaceFillingCurve&   modifyCurve() {return curve; }
+   __device__ __host__ SpaceFillingCurve&   modifyCurve() 
+   {return curve; }
    
   private:
       BitPacking packing;
   public:
-    __device__ __host__ const BitPacking& getPacking() const {return packing; }
-    __device__ __host__ BitPacking& modifyPacking() {return packing;}
+    __device__ __host__ const BitPacking& getPacking() const 
+    {return packing; }
+    __device__ __host__ BitPacking& modifyPacking() 
+    {return packing;}
   private:
     bool useGPUForOverhead;
   public:
-    bool isONGPUForOverhead() const  {return useGPUForOverhead;}
-    void setOnGPUForOverhead(const bool useGPUForOverhead_){useGPUForOverhead=useGPUForOverhead_;}
+    bool isONGPUForOverhead() const 
+    {return useGPUForOverhead;}
+    void setOnGPUForOverhead(const bool useGPUForOverhead_)
+    {useGPUForOverhead=useGPUForOverhead_;}
   
 };
 #endif /*LEMONADEGPU_CORE_METHOD_H_*/
