18b40c45f604e59941aaef5a380242f2868f9a9a
Toni Mueller <mueller-toni@ipfdd.de>
2020-12-02 07:48:15 +0100

    prettify

--- projects/pscbfm/mainCUDASimulatorGPUScBFM.cpp
+++ projects/pscbfm/mainCUDASimulatorGPUScBFM.cpp
@@ -1,5 +1,5 @@
 
-#include <chrono>                       // std::chrono::high_resolution_clock
+#include <chrono> // std::chrono::high_resolution_clock
 #include <cstring>
 //#include <cstdint>                      // uint32_t (C++11)
 #include <iostream>
@@ -20,7 +20,6 @@
 #include <LeMonADE/updater/UpdaterSimpleSimulator.h>
 #include <LeMonADE/feature/FeatureConnectionSc.h>
 
-
 #include <LeMonADEGPU/core/GPUScBFM.h>
 #include <LeMonADEGPU/utility/SelectiveLogger.hpp> // __FILENAME__
 
@@ -28,7 +27,7 @@
 #include "../analyzer/AnalyzerMonomerMSD.h"
 #include "../analyzer/AnalyzerSystemMSD.h"
 
-void printHelp( void )
+void printHelp(void)
 {
     std::stringstream msg;
     msg << "usage: ./SimulatorCUDAGPUScBFM [options]\n"
@@ -49,95 +48,116 @@ void printHelp( void )
         << "        Save after every <integer> Monte-Carlo steps to the output file.\n"
         << "    -o, --output <file path>\n"
         << "        All intermediate steps at each save-interval will be appended to this file even if it already exists\n"
-	<< "    -a, --analyze-MSD-ON <bool 0/1>\n"
+        << "    -a, --analyze-MSD-ON <bool 0/1>\n"
         << "        Analyzes the MSD of the whole system, the cross links and all monomers.\n"
-        << "    -v, --version\n"
-        ;
+        << "    -v, --version\n";
     std::cout << msg.str();
 }
 
-int main( int argc, char ** argv )
+int main(int argc, char **argv)
 {
     using hrclock = std::chrono::high_resolution_clock;
     auto const tProgram0 = hrclock::now();
 
-    std::string infile; /* BFM file containing positions of monomers and their connections */
-    std::string outfile      = "outfile.bfm"; /* at save_interval steps the current state of the simulation will be written to this file */
-    uint32_t max_mcs         = 0; /* how many Monte-Carlo steps to simulate */
-    uint32_t save_interval   = 0;
-    int      iGpuToUse       = 0;
-    int      iRngToUse       = -1;
+    std::string infile;                  /* BFM file containing positions of monomers and their connections */
+    std::string outfile = "outfile.bfm"; /* at save_interval steps the current state of the simulation will be written to this file */
+    uint32_t max_mcs = 0;                /* how many Monte-Carlo steps to simulate */
+    uint32_t save_interval = 0;
+    int iGpuToUse = 0;
+    int iRngToUse = -1;
     std::string seedFileName = "";
-    int      nSplitColors    = 0;
-    bool analyzeON=false;
-    bool diagMovesOn = false; 
-    
+    int nSplitColors = 0;
+    bool analyzeON = false;
+    bool diagMovesOn = false;
+
     try
     {
 
-        if ( argc <= 1 )
+        if (argc <= 1)
         {
             printHelp();
             return 0;
         }
 
         /* Parse command line arguments */
-        while ( true )
+        while (true)
         {
             static struct option long_options[] = {
-                { "colors"       , required_argument, 0, 'c' },
-                { "seeds"        , required_argument, 0, 'e' },
-                { "gpu"          , required_argument, 0, 'g' },
-                { "help"         , no_argument      , 0, 'h' },
-                { "initial-state", required_argument, 0, 'i' },
-		{ "diagMovesON"  , required_argument, 0, 'd' },
-                { "max-mcs"      , required_argument, 0, 'm' },
-                { "output"       , required_argument, 0, 'o' },
-                { "rng"          , required_argument, 0, 'r' },
-		{ "analyze"      , required_argument, 0, 'a' },
-                { "save-interval", required_argument, 0, 's' },
-                { 0, 0, 0, 0 }    // signal end of list
+                {"colors", required_argument, 0, 'c'},
+                {"seeds", required_argument, 0, 'e'},
+                {"gpu", required_argument, 0, 'g'},
+                {"help", no_argument, 0, 'h'},
+                {"initial-state", required_argument, 0, 'i'},
+                {"diagMovesON", required_argument, 0, 'd'},
+                {"max-mcs", required_argument, 0, 'm'},
+                {"output", required_argument, 0, 'o'},
+                {"rng", required_argument, 0, 'r'},
+                {"analyze", required_argument, 0, 'a'},
+                {"save-interval", required_argument, 0, 's'},
+                {0, 0, 0, 0} // signal end of list
             };
             /* getopt_long stores the option index here. */
             int option_index = 0;
-            int c = getopt_long( argc, argv, "c:e:g:hi:dm:o:r:a:s:", long_options, &option_index );
+            int c = getopt_long(argc, argv, "c:e:g:hi:dm:o:r:a:s:", long_options, &option_index);
 
-            if ( c == -1 )
+            if (c == -1)
                 break;
 
-            switch ( c )
+            switch (c)
             {
-                case 'c': nSplitColors  = std::atoi  ( optarg ); break;
-                case 'e': seedFileName  = std::string( optarg ); break;
-                case 'h': printHelp(); return 0;
-                case 'g': iGpuToUse     = std::atoi  ( optarg ); break;
-                case 'i': infile        = std::string( optarg ); break;
-		case 'd': diagMovesOn   = true                 ; break;
-                case 'm': max_mcs       = std::atol  ( optarg ); break;
-                case 'o': outfile       = std::string( optarg ); break;
-                case 'r': iRngToUse     = std::atoi  ( optarg ); break;
-		case 'a': analyzeON     = std::atoi  ( optarg ); break;
-                case 's': save_interval = std::atol  ( optarg ); break;
-                    break;
-                default:
-                    std::cerr << "Unknown option encountered: " << optarg << "\n";
-                    return 1;
+            case 'c':
+                nSplitColors = std::atoi(optarg);
+                break;
+            case 'e':
+                seedFileName = std::string(optarg);
+                break;
+            case 'h':
+                printHelp();
+                return 0;
+            case 'g':
+                iGpuToUse = std::atoi(optarg);
+                break;
+            case 'i':
+                infile = std::string(optarg);
+                break;
+            case 'd':
+                diagMovesOn = true;
+                break;
+            case 'm':
+                max_mcs = std::atol(optarg);
+                break;
+            case 'o':
+                outfile = std::string(optarg);
+                break;
+            case 'r':
+                iRngToUse = std::atoi(optarg);
+                break;
+            case 'a':
+                analyzeON = std::atoi(optarg);
+                break;
+            case 's':
+                save_interval = std::atol(optarg);
+                break;
+                break;
+            default:
+                std::cerr << "Unknown option encountered: " << optarg << "\n";
+                return 1;
             }
         }
-	std::cout << "Diagonal moves are turned " << diagMovesOn <<std::endl; 
+        std::cout << "Diagonal moves are turned " << diagMovesOn << std::endl;
         /* seed the globally available random number generators */
         RandomNumberGenerators rng;
-//         if ( ! seedFileName.empty() )
-//             rng.seedAll( seedFileName );
-//         else
-            rng.seedAll();
+        //         if ( ! seedFileName.empty() )
+        //             rng.seedAll( seedFileName );
+        //         else
+        rng.seedAll();
 
         /* Check the initial values. Note that the drawing of these random
          * values can't be omitted, or else all subsequent random numbers
          * will shift / change! */
-        std::rand      ();
+        std::rand();
         rng.r250_rand32();
-        rng.r250_drand ();
+        rng.r250_drand();
 
         /*
         FeatureExcludedVolume<> is equivalent to FeatureExcludedVolume< FeatureLattice< bool > >
@@ -145,13 +165,13 @@ int main( int argc, char ** argv )
             FeatureLattice< uint8_t > FeatureExcludedVolume< FeatureLatticePowerOfTwo<> > )
             Features;
         */
-//         typedef LOKI_TYPELIST_4( FeatureMoleculesIO, FeatureAttributes<>,
-//                                  FeatureExcludedVolumeSc<>, FeatureConnectionSc ) Features;
-        typedef LOKI_TYPELIST_4( FeatureMoleculesIOUnsaveCheck, FeatureAttributes<>, FeatureExcludedVolumeSc<>, FeatureConnectionSc ) Features;
-// 	typedef LOKI_TYPELIST_3( FeatureMoleculesIOUnsaveCheck, FeatureAttributes<>, FeatureConnectionSc ) Features;
-				 
-        typedef ConfigureSystem< VectorInt3, Features, 8 > Config;
-        typedef Ingredients< Config > Ing;
+        //         typedef LOKI_TYPELIST_4( FeatureMoleculesIO, FeatureAttributes<>,
+        //                                  FeatureExcludedVolumeSc<>, FeatureConnectionSc ) Features;
+        typedef LOKI_TYPELIST_4(FeatureMoleculesIOUnsaveCheck, FeatureAttributes<>, FeatureExcludedVolumeSc<>, FeatureConnectionSc) Features;
+        // 	typedef LOKI_TYPELIST_3( FeatureMoleculesIOUnsaveCheck, FeatureAttributes<>, FeatureConnectionSc ) Features;
+
+        typedef ConfigureSystem<VectorInt3, Features, 8> Config;
+        typedef Ingredients<Config> Ing;
         Ing myIngredients;
 
         /**
@@ -162,49 +182,50 @@ int main( int argc, char ** argv )
          * it on the heap, i.e.:
          *   GPUScBFM<Ing> gpuBfm( myIngredients, save_interval, iGpuToUse );
          */
-        auto const pUpdaterGpu = new GPUScBFM<Ing>( myIngredients, save_interval, diagMovesOn);
-        pUpdaterGpu->setGpu( iGpuToUse );
-        pUpdaterGpu->activateLogging( "Error"     );
+        auto const pUpdaterGpu = new GPUScBFM<Ing>(myIngredients, save_interval, diagMovesOn);
+        pUpdaterGpu->setGpu(iGpuToUse);
+        pUpdaterGpu->activateLogging("Error");
         //pUpdaterGpu->activateLogging( "Stats"      );
-        pUpdaterGpu->activateLogging( "Info"      );
-// 	pUpdaterGpu->activateLogging( "Check"      );
-        if ( nSplitColors > 0 )
-            pUpdaterGpu->setSplitColors( nSplitColors );
+        pUpdaterGpu->activateLogging("Info");
+        // 	pUpdaterGpu->activateLogging( "Check"      );
+        if (nSplitColors > 0)
+            pUpdaterGpu->setSplitColors(nSplitColors);
 
         TaskManager taskmanager;
-        taskmanager.addUpdater( new UpdaterReadBfmFile<Ing>( infile, myIngredients,UpdaterReadBfmFile<Ing>::READ_LAST_CONFIG_SAVE ), 0 );
+        taskmanager.addUpdater(new UpdaterReadBfmFile<Ing>(infile, myIngredients, UpdaterReadBfmFile<Ing>::READ_LAST_CONFIG_SAVE), 0);
         //here you can choose to use MoveLocalBcc instead. Careful though: no real tests made yet
         //(other than for latticeOccupation, valid bonds, frozen monomers...)
-        taskmanager.addUpdater( pUpdaterGpu );
-	if (analyzeON)
-	{
-	  taskmanager.addAnalyzer( new AnalyzerSystemMSD   <Ing>( myIngredients, 0       ) );
-	  taskmanager.addAnalyzer( new AnalyzerMonomerMSD  <Ing>( myIngredients, 0       ) );
-	  taskmanager.addAnalyzer( new AnalyzerCrossLinkMSD<Ing>( myIngredients, 0       ) );
-	}
-        taskmanager.addAnalyzer( new AnalyzerWriteBfmFile<Ing>( outfile, myIngredients ) );
-        taskmanager.addAnalyzer( new AnalyzerWriteBfmFile<Ing>( "LastConfig.bfm", myIngredients, AnalyzerWriteBfmFile<Ing>::OVERWRITE ) );
+        taskmanager.addUpdater(pUpdaterGpu);
+        if (analyzeON)
+        {
+            taskmanager.addAnalyzer(new AnalyzerSystemMSD<Ing>(myIngredients, 0));
+            taskmanager.addAnalyzer(new AnalyzerMonomerMSD<Ing>(myIngredients, 0));
+            taskmanager.addAnalyzer(new AnalyzerCrossLinkMSD<Ing>(myIngredients, 0));
+        }
+        taskmanager.addAnalyzer(new AnalyzerWriteBfmFile<Ing>(outfile, myIngredients));
+        taskmanager.addAnalyzer(new AnalyzerWriteBfmFile<Ing>("LastConfig.bfm", myIngredients, AnalyzerWriteBfmFile<Ing>::OVERWRITE));
 
         taskmanager.initialize();
 
         auto const tTasks0 = hrclock::now();
-        taskmanager.run( max_mcs / save_interval );
+        taskmanager.run(max_mcs / save_interval);
 
         auto const tTasks1 = hrclock::now();
         std::stringstream sBuffered;
-        sBuffered << "tTaskLoop = " << std::chrono::duration<double>( tTasks1 - tTasks0 ).count() << "s\n";
+        sBuffered << "tTaskLoop = " << std::chrono::duration<double>(tTasks1 - tTasks0).count() << "s\n";
         std::cerr << sBuffered.str();
 
         taskmanager.cleanup();
     }
-    catch( std::exception const & e )
+    catch (std::exception const &e)
     {
-        std::cerr << "[" << __FILENAME__ << "] Caught exception: " << e.what() << std::endl;;
+        std::cerr << "[" << __FILENAME__ << "] Caught exception: " << e.what() << std::endl;
+        ;
     }
 
     auto const tProgram1 = hrclock::now();
     std::stringstream sBuffered;
-    sBuffered << "tProgram = " << std::chrono::duration<double>( tProgram1 - tProgram0 ).count() << "s\n";
+    sBuffered << "tProgram = " << std::chrono::duration<double>(tProgram1 - tProgram0).count() << "s\n";
     std::cerr << sBuffered.str();
     return 0;
 }
